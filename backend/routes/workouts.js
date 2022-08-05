235e7f3b53dc7b6832d262ec65865f0f287b954f
hasindusithmin <hasindusithmin64@gmail.com>
2022-08-05 13:03:49 +0530

    Update workouts.js

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,15 +1,25 @@
 const express = require('express')
 const WorkOut = require('../models/workoutModel')
 const router = express.Router()
+const mongoose = require('mongoose')
 
 // GET all workouts
-router.get('/', (req, res) => {
-  res.json({mssg: 'GET all workouts'})
+router.get('/', async(req, res) => {
+    const workouts = await WorkOut.find({}).sort({createdAt:-1})
+    res.status(200).json(workouts)
 })
 
 // GET a single workout
-router.get('/:id', (req, res) => {
-  res.json({mssg: 'GET a single workout'})
+router.get('/:id', async(req, res) => {
+  const {id} = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).json({error:'No such workout'})
+
+  const workout = await WorkOut.findById(id)
+
+  if (!workout) return res.status(404).json({error:'No such workout'})
+
+  res.status(200).json(workout)
 })
 
 // POST a new workout
@@ -21,17 +31,32 @@ router.post('/', async(req, res) => {
   } catch (error) {
     res.status(400).json({error:error.message})
   }
-
 })
 
 // DELETE a workout
-router.delete('/:id', (req, res) => {
-  res.json({mssg: 'DELETE a workout'})
+router.delete('/:id', async(req, res) => {
+  const {id} = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).json({error:'No such workout'})
+
+  const workout = await WorkOut.findByIdAndDelete({_id:id})
+
+  if (!workout) return res.status(404).json({error:'No such workout'})
+
+  res.status(202).json(workout)
 })
 
 // UPDATE a workout
-router.patch('/:id', (req, res) => {
-  res.json({mssg: 'UPDATE a workout'})
+router.patch('/:id', async(req, res) => {
+  const {id} = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).json({error:'No such workout'})
+
+  const workout = await WorkOut.findByIdAndUpdate({_id:id},{...req.body})
+
+  if (!workout) return res.status(404).json({error:'No such workout'})
+
+  res.status(202).json(workout)
 })
 
 module.exports = router 

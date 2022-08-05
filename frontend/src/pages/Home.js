bfbece14aff36c068e804ace82cfbc735113a73b
hasindusithmin <hasindusithmin64@gmail.com>
2022-08-05 14:45:56 +0530

    Render workouts..

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react"
+import WorkoutDetails from "../components/WorkoutDetails"
 
 const Home = () => {
+
+    const [workouts,setWorkOuts] = useState(null)
+
+    useEffect(()=>{
+        const fetchWorkOuts = async()=>{
+            const res = await fetch("/api/workoouts")
+            const data = await res.json()
+            if (res.ok) setWorkOuts(data)
+        }
+        fetchWorkOuts()
+    },[])
 
     return (
         <div className="home">
             <h2>Home</h2>
+            <div className="workouts">
+                {workouts && workouts.map(workout=>(
+                    <WorkoutDetails workout={workout} />
+                ))}
+            </div>
         </div>
     )
 }

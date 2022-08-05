4de67d779835a236f3e4360d4496b72ff61bf31b
hasindusithmin <hasindusithmin64@gmail.com>
2022-08-05 12:33:59 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,23 +2,32 @@ require('dotenv').config()
 
 const express = require('express')
 const workOutRoute = require('./routes/workouts')
+const mongoose = require('mongoose')
 // express app
 const app = express()
 
 // middleware
 app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
+    console.log(req.path, req.method)
+    next()
 })
 // middleware
-app.use("/api/workout",workOutRoute)
+app.use("/api/workouts", workOutRoute)
 
 // routes(root)
 app.get('/', (req, res) => {
-  res.json({mssg: 'Welcome to the app'})
+    res.json({ mssg: 'Welcome to the app' })
 })
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log('listening on port', process.env.PORT)
-}) 
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('connected to database')
+        // listen to port
+        app.listen(process.env.PORT, () => {
+            console.log('listening for requests on port', process.env.PORT)
+        })
+    })
+    .catch((err) => {
+        console.log(err)
+    }) 
